8a9a21eda4d8af1a1f52917de577b91a991b8282
Stepan Denysenko <60338342+stepden04@users.noreply.github.com>
2026-04-16 20:25:41 +0300

    feat: added skeletons for required endpoints

--- app/main.py
+++ app/main.py
@@ -20,8 +20,24 @@ def health() -> dict[str, str]:
     return {"status": "ok"}
 
 
+@app.post("/api/requests")
+def new_request(): ...
+
+
+@app.get("/api/requests")
+def get_requests(): ...
+
+
+@app.get("/api/requests/room/<room>")
+def get_requests_by_room(room: str): ...
+
+
+@app.patch("/api/requests/<request_id>")
+def update_request(request_id: str): ...
+
+
 @app.post("/api/voice_to_text")
-def voice_to_text(files: UploadFile) ->  dict[str, str | list[typing.Any]]:
+def voice_to_text(files: UploadFile) -> dict[str, str | list[typing.Any]]:
     filename = files.filename or f"{uuid4().hex}.mp3"
 
     file = Path("tmp") / filename
@@ -35,7 +51,8 @@ def voice_to_text(files: UploadFile) ->  dict[str, str | list[typing.Any]]:
     finally:
         file.unlink()
 
+
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000) # noqa: S104
+    uvicorn.run(app, host="0.0.0.0", port=8000)  # noqa: S104
