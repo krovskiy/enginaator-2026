710cd8d5c1e7721fa0cdbd074df5c976003ad339
Stepan Denysenko <60338342+stepden04@users.noreply.github.com>
2026-04-16 20:02:33 +0300

    feat: add whisper support; fix: fix linter warnings

--- app/main.py
+++ app/main.py
@@ -1,4 +1,11 @@
-from fastapi import FastAPI
+import typing
+from pathlib import Path
+from uuid import uuid4
+
+import whisper
+from fastapi import FastAPI, UploadFile
+
+model = whisper.load_model("base")
 app = FastAPI(title="SVARA Room Service API")
 
 # app.include_router(requests_router, prefix="/api/requests", tags=["requests"])
@@ -9,5 +16,26 @@ app = FastAPI(title="SVARA Room Service API")
 
 
 @app.get("/api/health")
-def health():
+def health() -> dict[str, str]:
     return {"status": "ok"}
+
+
+@app.post("/api/voice_to_text")
+def voice_to_text(files: UploadFile) ->  dict[str, str | list[typing.Any]]:
+    filename = files.filename or f"{uuid4().hex}.wav"
+
+    file = Path("tmp") / filename
+    file.parent.mkdir(exist_ok=True)
+    with file.open("wb") as buffer:
+        buffer.write(files.file.read())
+    try:
+        return model.transcribe(audio=file.as_posix())
+    except Exception as e:
+        return {"error": str(e)}
+    finally:
+        file.unlink()
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000) # noqa: S104
